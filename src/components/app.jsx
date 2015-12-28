5afcbd347cd4a715d5304381cbccd1a97893acbf
jennyyuejin <jin.yuejenny@gmail.com>
2015-12-28 02:14:13 -0800

    bundle.js created successfully

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render() {
+        return (
+            <div>
+                Hello JJ's World
+            </div>
+        );
+    }
+});
+
---
+++ src/entry.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jj on 12/23/15.
+ */
+
+var React = require('react');
+var App = require('./components/app.jsx');
+
+React.render(App, document.body);
